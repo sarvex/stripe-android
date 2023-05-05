cf475d39651f28a78bcdc0939b728c13ae367e28
jameswoo-stripe <99316447+jameswoo-stripe@users.noreply.github.com>
2023-05-05 13:48:07 -0700

    Add us_bank_account LPM filter

--- payments-core/src/main/java/com/stripe/android/model/StripeIntent.kt
+++ payments-core/src/main/java/com/stripe/android/model/StripeIntent.kt
@@ -73,6 +73,9 @@ sealed interface StripeIntent : StripeModel {
 
     fun requiresConfirmation(): Boolean
 
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+    fun getPaymentMethodOptions(): Map<String, Any?>
+
     /**
      * Type of the next action to perform.
      */
